12e09a7737dbc546108c8461c05cf56ca2074376
peimanr <peiman_ing@yahoo.ca>
2017-11-08 13:57:47 -0800

    first commit

---
+++ bookstore/src/main/java/com/bookstore/repository/CartItemRepository.java
@@ -0,0 +1,14 @@
+package com.bookstore.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bookstore.domain.CartItem;
+import com.bookstore.domain.ShoppingCart;
+
+@Transactional
+public interface CartItemRepository extends CrudRepository<CartItem, Long>{
+	List<CartItem> findByShoppingCart(ShoppingCart shoppingCart);
+}
